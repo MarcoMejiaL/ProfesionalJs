36ab9bd045ece77fd25777155f93e9de1665accb
Marco Mejia <tomatin77@gmail.com>
2021-07-18 21:41:52 -0500

    generators

--- 08generators/index.js
+++ 08generators/index.js
@@ -1,6 +1,27 @@
-function* simpleGenerator(){
+/* function* simpleGenerator(){
     console.log('start');
+    yield
     console.log('end');
 }
 
 const gen = simpleGenerator()
+ */
+/* function* idmaker(){
+    let id =1;
+    while(id <=2){
+        yield id
+        id = id +1;
+    }
+}
+const gen =idmaker(); */
+function* ejemplo(){
+    let a =1;
+    let b =1;
+    while(true){
+        const nextNumber = a+b;
+        a = b;
+        b= nextNumber;
+        yield nextNumber;
+
+    }
+}
